183379ab5be05ac35beabf7985d27f7eb155d544
LineSurfer <114391586+LineSurfer@users.noreply.github.com>
2023-07-19 01:57:35 +0800

    Day 1

---
+++ Day 1 - Band Name Generator.py
@@ -0,0 +1,45 @@
+# import time to use the typewriter delay effect
+import time
+
+# define typewriter effect to apply it to whole code
+
+
+def typewriter_print(text, delay=0.03):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+
+
+# while true to loop the code endlessly until "exit" is input by the user
+while True:
+    typewriter_print("Greetings! I am the greatest Band Name Generator.\n")
+
+    time.sleep(0.3)  # Delay before asking for the first name
+
+    typewriter_print("\nCan I ask what is your first name?\n")
+    first_name = input()
+    if first_name == "exit":
+        break
+
+    time.sleep(0.3)  # Delay before acknowledging the name
+
+    typewriter_print("\nGreat! Hello " + first_name +
+                     "! That's a beautiful name.\n")
+
+    time.sleep(0.3)  # Delay before asking for the city name
+
+    typewriter_print("\nIn what city did you grow up?\n")
+    city_name = input()
+
+    time.sleep(0.3)  # Delay before acknowledging the city name
+
+    typewriter_print(
+        "\nWell, that's a fantastic city, isn't it? " + city_name + "!\n")
+
+    band_name = first_name + " " + city_name + "!"
+    typewriter_print("\nYour amazing band name will be " + band_name + "\n")
+
+    time.sleep(0.3)  # Delay before displaying the final message
+
+    typewriter_print(
+        "\nThanks for using Band Name Generator. Hope you liked it!\n\n")
